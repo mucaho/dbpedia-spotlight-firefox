713d525d4b4e759bb20c734af625684221ab6be4
mucaho <mkucko@gmail.com>
2015-05-31 10:32:41 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# dbpedia-spotlight-firefox
-Firefox addon that annotates entities using DBPedia Spotlight service.
+# DBPedia Spotlight Firefox addon #
+Firefox addon that annotates entities using [DBPedia Spotlight service](https://github.com/dbpedia-spotlight/dbpedia-spotlight/wiki).
+## Usage ##
+Select text content then go to the context menu (usually Right-Click):
+* _"Annotate using DBPedia Spotlight..."_ replaces the currently selected HTML with annotated HTML.
+* _"(Poput) Annotate using DBPedia Spotlight..."_ provides annotated text inside a popup (useful for non-HTML text content).
---
+++ index.js
@@ -0,0 +1,83 @@
+var self = require("sdk/self");
+var Request = require("sdk/request").Request;
+var selection = require("sdk/selection");
+
+var panel = require("sdk/panel").Panel({
+  width: 400,
+  height: 300,
+  onHide: function() {
+    panel.port.emit("replace", "<div></div>");
+  },
+  contentURL: "./panel.html",
+  contentScript: "self.port.on('replace', function onShow(data) {" +
+                 "  document.body.innerHTML = data;" +
+                 "});" +
+                 "self.port.on('append', function onShow(data) {" +
+                 "  var body = document.getElementById('body');" +
+                 "  var newcontent = document.createElement('div');" +
+                 "  newcontent.innerHTML = data;" +
+                 "  while (newcontent.firstChild)" +
+                 "    body.appendChild(newcontent.firstChild);" +
+                 "});"
+});
+
+
+
+function Item (label, inputCB, outputCB) {
+  return {
+    label: label + "Annotate using DBPedia Spotlight...",
+    context: cm.SelectionContext(),
+    image: self.data.url("icon-16.png"),
+    contentScript: "self.on('click', function (node, data) {" +
+                   "  self.postMessage(node.textContent);" +
+                   "});",
+    onMessage: function (text) {
+      if (selection.isContiguous) {
+        annotate(selection, inputCB, outputCB);
+      } else {
+        for (var subselection in selection) {
+           annotate(subselection, inputCB, outputCB);
+        }
+      }
+    }
+  };
+}
+
+
+function annotate (selection, inputCB, outputCB) {
+  Request({
+    url: "http://spotlight.dbpedia.org/rest/annotate",
+    content: {
+      text: inputCB(selection)
+    },
+    headers: {
+      "Accept": "application/xhtml+xml"
+    },
+    onComplete: function (response) {
+      if (response.status === 200)
+        outputCB(selection, response.text);
+    }
+  }).post();
+}
+
+
+
+var cm = require("sdk/context-menu");
+/***********************
+ * IN-PLACE ANNOTATION *
+ ***********************/
+cm.Item(Item("", function (selection) {
+    return selection.html;
+}, function (selection, text) {
+    if (selection.html)
+      selection.html = text
+}));
+/**********************
+ * POPUP ANNOTATION   *
+ **********************/
+cm.Item(Item("(Popup) ", function (selection) {
+    return selection.text;
+}, function (selection, text) {
+    panel.port.emit("append", text);
+    panel.show();
+}));
